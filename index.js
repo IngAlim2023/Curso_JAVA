1984e1deae21a796a05e08527d8edae7e729a635
IngAlim2023 <enrique.porras@udea.edu.co>
2024-07-31 07:28:40 -0500

    Videos modales todos

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Video Jorge
 let videoUno = document.getElementById("modal-1");
 let target = document.getElementById("jorge");
 let closeVu = document.getElementById("close-j");
@@ -7,4 +8,28 @@ target.addEventListener('click', ()=>{
 })
 closeVu.addEventListener('click', ()=>{
     videoUno.close();
+})
+
+//Video Andres
+let videoDos = document.getElementById("modal-2");
+let target2 = document.getElementById("andres");
+let closeVd = document.getElementById("close-a");
+
+target2.addEventListener('click', ()=>{
+    videoDos.showModal();
+})
+closeVd.addEventListener('click', ()=>{
+    videoDos.close();
+})
+
+//Video Jeison
+let videoTre = document.getElementById("modal-3");
+let target3 = document.getElementById("jeison");
+let closeVt = document.getElementById("close-je");
+
+target3.addEventListener('click', ()=>{
+    videoTre.showModal();
+})
+closeVt.addEventListener('click', ()=>{
+    videoTre.close();
 })
